f8badbe5b505844d586554b506dfd4b2bf5d4cb2
พัชรพล ศรีดา <muc5674@gmail.com>
2024-03-27 14:57:19 +0700

    qeri7uo89p

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,13 @@
 module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_qvxLTiJrZ48oWgMFBBKHVxvjNHoI",
+      apiToken: "DhsrK8OvXuJm1q2b1d9neiHV",
+      appFilter: "cms-emfarm",
+      teamFilter: "",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
   },
 });
